ca4f84d568abb16eabdcd177212f4c36fd0b8512
hezongxi <1027520304@qq.com>
2022-06-18 16:46:29 +0800

    上影推荐组件

---
+++ src/views/movieRecommendation/index.vue
@@ -0,0 +1,51 @@
+<!--  -->
+<template>
+  <div>
+    <br>
+    <div class="container">
+     <h1><br>&nbsp;上影推荐</h1></h1>
+     <p>影片最让人赞叹的地方，是将现实境遇只作为宽泛的背景，而没有形塑为因果关系的本源。谁说爱情必然要出轨、堕胎、绝症，爱情里的荡气回肠或是惊心动魄，从来都只在男女之间的爱的感受...<span>展开</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+div{
+        h1{
+        font-size: 0.35rem;
+        border-top:solid #eee 0.01rem;
+        margin-left: 0.3rem;
+        margin-right: 0.3rem;
+    }
+}
+.container{
+    border-top: solid #eee 0.02rem;
+    p{
+    padding: 0.45rem;
+    padding-top: 0.3rem;
+    line-height: 0.5rem;
+    span{
+        font-size: 0.27rem;
+        color:#86b6f9ec;
+    }
+}
+}
+
+</style>
